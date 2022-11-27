e4966926acb9497262cb199f86d2747cf2587da4
Yama <yassin.makhloufi@voiapp.io>
2022-11-27 15:09:26 +0100

    enable access for aws dynamoDB via iam serviceaccount

--- src/dynamodb.php
+++ src/dynamodb.php
@@ -5,17 +5,14 @@ declare(strict_types=1);
 require_once 'vendor/autoload.php';
 
 use Aws\DynamoDb\DynamoDbClient;
+use Aws\DynamoDb\Marshaler;
 use Illuminate\Support\Str;
 use Ramsey\Uuid\Uuid;
 
 print getDynamoResults([
-    'region'      => $_ENV['AWS_REGION'],
-    'version'     => 'latest',
-    'endpoint'    => $_ENV['DYNAMODB_ENDPOINT'],
-    'credentials' => [
-        'key'    => $_ENV['AWS_ACCESS_KEY_ID'],
-        'secret' => $_ENV['AWS_SECRET_ACCESS_KEY'],
-    ],
+    'region'   => $_ENV['AWS_REGION'] ?? "",
+    'version'  => 'latest',
+    'endpoint' => $_ENV['DYNAMODB_ENDPOINT'],
 ]);
 
 function getDynamoResults(array $connectionParams)
@@ -36,11 +33,12 @@ function getDynamoResults(array $connectionParams)
     }
 
 
+    echo "fetching data from DynamoDB...";
     $output   .= "<ul>";
-    $iterator = $client->getIterator('Scan', ['TableName' => 'animal_customer']);
+    $iterator = $client->getIterator('Scan', ['TableName' => 'my-awesome-project.animal_customer']);
     foreach ($iterator as $row) {
         foreach ($row['pets']['L'] as $pet) {
-            $pet = $pet['M'];
+            $pet    = $pet['M'];
             $output .= "<li>";
             $output .= htmlspecialchars(sprintf("%s %s has %u %s %s %s %s.",
                 $row['first_name']['S'],
@@ -64,124 +62,126 @@ function migrateIfNecessary(DynamoDbClient $client)
     $iterator = $client->getIterator('ListTables');
 
     foreach ($iterator as $tableName) {
-        if ($tableName === 'animal_customer') {
+        if ($tableName === 'my-awesome-project.animal_customer') {
             return; // no need to migrate
         }
     }
 
 
     $client->createTable([
-        'TableName'             => 'animal_customer',
-        'AttributeDefinitions'  => [
+        'TableName'            => 'my-awesome-project.animal_customer',
+        'AttributeDefinitions' => [
             [
                 'AttributeName' => 'id',
                 'AttributeType' => 'S',
             ],
         ],
-        'KeySchema'             => [
+        'KeySchema'            => [
             [
                 'AttributeName' => 'id',
                 'KeyType'       => 'HASH',
             ],
         ],
-        'BillingMode' => 'PAY_PER_REQUEST',
+        'BillingMode'          => 'PAY_PER_REQUEST',
     ]);
 
     $client->waitUntil('TableExists', [
-        'TableName' => 'animal_customer',
+        'TableName' => 'my-awesome-project.animal_customer',
+    ]);
+
+    $marshaler = new Marshaler();
+    $client->putItem([
+        'TableName' => 'my-awesome-project.animal_customer',
+        'Item'      => $marshaler->marshalJson(json_encode([
+            'id'         => Uuid::uuid4()->toString(),
+            'phone'      => "+46 - 72 886 1234",
+            'first_name' => 'Dwayne',
+            'last_name'  => 'Johnson',
+            'pets'       => [
+                [
+                    'count'   => rand(1, 5),
+                    'id'      => 1,
+                    'species' => 'feline',
+                    'name'    => 'bobcat',
+                    'color'   => 'beige-ish?',
+                    'has_fur' => true,
+                ],
+                [
+                    'count'   => rand(1, 5),
+                    'id'      => 2,
+                    'species' => 'feline',
+                    'name'    => 'panther',
+                    'color'   => 'black',
+                    'has_fur' => true,
+                ],
+            ],
+        ])),
     ]);
 
     $client->putItem([
-        'TableName' => 'animal_customer',
-        'Item'      => [
-            'id'         => ['S' => Uuid::uuid4()->toString()],
-            'phone'      => ['S' => "+46 - 72 886 1234"],
-            'first_name' => ['S' => 'Dwayne'],
-            'last_name'  => ['S' => 'Johnson'],
-            'pets'       => ['L' => [
-                ['M' => [
-                    'count'   => ['N' => rand(1, 5)],
-                    'id'      => ['N' => 1],
-                    'species' => ['S' => 'feline'],
-                    'name'    => ['S' => 'bobcat'],
-                    'color'   => ['S' => 'beige-ish?'],
-                    'has_fur' => ['BOOL' => true],
-                ]], ['M' => [
-                    'count'   => ['N' => rand(1, 5)],
-                    'id'      => ['N' => 2],
-                    'species' => ['S' => 'feline'],
-                    'name'    => ['S' => 'panther'],
-                    'color'   => ['S' => 'black'],
-                    'has_fur' => ['BOOL' => true],
-                ]],
-            ]],
-        ],
+        'TableName' => 'my-awesome-project.animal_customer',
+        'Item'      => $marshaler->marshalJson(json_encode([
+            'id'         => Uuid::uuid4()->toString(),
+            'phone'      => '+1 - 555 1234',
+            'first_name' => 'John',
+            'last_name'  => 'Cena',
+            'pets'       => [
+                [
+                    'count'   => rand(1, 5),
+                    'id'      => 3,
+                    'species' => 'reptilian',
+                    'name'    => 'king cobra',
+                    'color'   => 'olive green',
+                    'has_fur' => false,
+                ],
+            ],
+        ])),
     ]);
 
     $client->putItem([
-        'TableName' => 'animal_customer',
-        'Item'      => [
-            'id'         => ['S' => Uuid::uuid4()->toString()],
-            'phone'      => ['S' => '+1 - 555 1234'],
-            'first_name' => ['S' => 'John'],
-            'last_name'  => ['S' => 'Cena'],
-            'pets'       => ['L' => [
-                ['M' => [
-                    'count'   => ['N' => rand(1, 5)],
-                    'id'      => ['N' => 1],
-                    'species' => ['S' => 'reptilian'],
-                    'name'    => ['S' => 'king cobra'],
-                    'color'   => ['S' => 'olive green'],
-                    'has_fur' => ['BOOL' => false],
-                ]],
-            ]],
-        ],
+        'TableName' => 'my-awesome-project.animal_customer',
+        'Item'      => $marshaler->marshalJson(json_encode([
+            'id'         => Uuid::uuid4()->toString(),
+            'phone'      => '+30 - 443 1122',
+            'first_name' => 'Steve',
+            'last_name'  => 'Austin',
+            'pets'       => [
+                [
+                    'count'   => rand(1, 5),
+                    'id'      => 4,
+                    'species' => 'amphibian',
+                    'name'    => 'poison dart frog',
+                    'color'   => 'red',
+                    'has_fur' => false,
+                ],
+            ],
+        ])),
     ]);
 
     $client->putItem([
-        'TableName' => 'animal_customer',
-        'Item'      => [
-            'id'         => ['S' => Uuid::uuid4()->toString()],
-            'phone'      => ['S' => '+30 - 443 1122'],
-            'first_name' => ['S' => 'Steve'],
-            'last_name'  => ['S' => 'Austin'],
-            'pets'       => ['L' => [
-                ['M' => [
-                    'count'   => ['N' => rand(1, 5)],
-                    'id'      => ['N' => 1],
-                    'species' => ['S' => 'amphibian'],
-                    'name'    => ['S' => 'poison dart frog'],
-                    'color'   => ['S' => 'red'],
-                    'has_fur' => ['BOOL' => false],
-                ]],
-            ]],
-        ],
-    ]);
-
-    $client->putItem([
-        'TableName' => 'animal_customer',
-        'Item'      => [
-            'id'         => ['S' => Uuid::uuid4()->toString()],
-            'phone'      => ['S' => '+555 - 998 1222'],
-            'first_name' => ['S' => 'Rey'],
-            'last_name'  => ['S' => 'Mysterio'],
-            'pets'       => ['L' => [
-                ['M' => [
-                    'count'   => ['N' => rand(1, 5)],
-                    'id'      => ['N' => 1],
-                    'species' => ['S' => 'canine'],
-                    'name'    => ['S' => 'fox'],
-                    'color'   => ['S' => 'orange'],
-                    'has_fur' => ['BOOL' => true],
-                ]], ['M' => [
-                    'count'   => ['N' => rand(1, 5)],
-                    'id'      => ['N' => 2],
-                    'species' => ['S' => 'canine'],
-                    'name'    => ['S' => 'chihuahua'],
-                    'color'   => ['S' => 'brown'],
-                    'has_fur' => ['BOOL' => true],
-                ]],
-            ]],
-        ],
+        'TableName' => 'my-awesome-project.animal_customer',
+        'Item'      => $marshaler->marshalJson(json_encode([
+            'id'         => Uuid::uuid4()->toString(),
+            'phone'      => '+555 - 998 1222',
+            'first_name' => 'Rey',
+            'last_name'  => 'Mysterio',
+            'pets'       => [
+                [
+                    'count'   => rand(1, 5),
+                    'id'      => 5,
+                    'species' => 'canine',
+                    'name'    => 'fox',
+                    'color'   => 'orange',
+                    'has_fur' => true,
+                ], [
+                    'count'   => rand(1, 5),
+                    'id'      => 6,
+                    'species' => 'canine',
+                    'name'    => 'chihuahua',
+                    'color'   => 'brown',
+                    'has_fur' => true,
+                ],
+            ],
+        ])),
     ]);
 }
