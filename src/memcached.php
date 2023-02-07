a324f66fe14d6df0f54f19c02dab226c96f7f126
Yama <yassin.makhloufi@voiapp.io>
2023-02-07 19:29:59 +0100

    add event support via AWS eventbridge

--- src/memcached.php
+++ src/memcached.php
@@ -1,29 +1,37 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 require_once 'vendor/autoload.php';
 
 $memcached = new Memcached;
-$memcached->addServer($_ENV['MEMCACHED_HOST'], intval($_ENV['MEMCACHED_PORT'])) or die ("Could not connect to Memcached host");
-echo "Connection to <b>Memcache</b> successful!<br/>\n<br/>\n";
+$connected = $memcached->addServer($_ENV['MEMCACHED_HOST'], intval($_ENV['MEMCACHED_PORT']));
+if (!$connected || $memcached->getAllKeys() === false){
+    echo "Connection to <b>Memcache</b> <span style='color: red'>failed!</span><ul><li>Couldn't connect to server!</li></ul>";
+    return;
+}
 
-
+echo "Connection to <b>Memcache</b> <span style='color: green'>successful!</span><ul>";
 $tmp_object = new stdClass;
 $tmp_object->str_attr = 'test';
 $tmp_object->int_attr = 123;
 
-$memcached->set('my_object', $tmp_object, 1) or die ("Failed to save data at the server");
-echo "Store data in the cache (data will expire in 1 second): ";
+$putSuccessful = $memcached->set('my_object', $tmp_object, 10);
+if (!$putSuccessful) {
+    echo "<li><span style='color: red'><b>Failed</b> to store object in memcached!</span></li>";
+    return;
+}
+
+echo "<li>Store data in the cache (data will expire in 1 second): ";
 echo json_encode($tmp_object);
 
 $get_result1 = $memcached->get('my_object');
-echo "<br/>\nData from the cache: ";
+echo "</li><li>Data from the cache: ";
 echo json_encode($get_result1);
 
 sleep(1);
-echo "<br/>\nSleep 1 second<br/>\n";
+echo "</li><li>Sleep 1 second</li>";
 
 $get_result2 = $memcached->get('my_object');
-echo "Data from the cache after one second (should be empty): ";
+echo "<li>Data from the cache after one second (should be empty): ";
 echo json_encode($get_result2);
+
+echo "</li></ul>";
--- src/postgres.php
+++ src/postgres.php
@@ -1,6 +1,4 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 require_once 'vendor/autoload.php';
 require_once 'sql_results.php';
